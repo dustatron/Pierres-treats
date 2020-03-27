a90f2795444dfd84148e32b82b8b1dea66fd821a
Dusty <dustymccord@gmail.com>
2020-03-27 08:38:44 -0700

    add placeholder crud methods for Flavors and Treats.

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -1,7 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace PierresTreats.Controllers
 {
-    public class FlavorsController
+  public class FlavorsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+
+    public FlavorsController(PierresTreatsContext db)
     {
-        
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor, int RecipeId)
+    {
+      return View();
+    }
+
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
+
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor, int TreatId)
+    {
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddTreat(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult AddTreat(Flavor flavor, int TreatId)
+    {
+      return View();
+    }
+
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteRecipes(int joinId)
+    {
+
+      return RedirectToAction("Index");
+    }
+  }
 }
--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -1,7 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace PierresTreats.Controllers
 {
-    public class TreatsController
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+
+    public TreatsController(PierresTreatsContext db)
     {
-        
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat, int FlavorId)
+    {
+      return View();
+    }
+
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
+
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat, int TreatId)
+    {
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddTreat(int id)
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult AddTreat(Treat treat, int FlavorId)
+    {
+      return View();
+    }
+
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteFlavors(int joinId)
+    {
+
+      return RedirectToAction("Index");
+    }
+  }
 }
