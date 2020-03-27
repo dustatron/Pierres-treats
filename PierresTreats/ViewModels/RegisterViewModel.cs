631e89b00254b68cf9d0961203103d9bd5eb59a3
Dusty <dustymccord@gmail.com>
2020-03-27 10:30:08 -0700

    add auth login and logout.

---
+++ PierresTreats/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PierresTreats.ViewModels
+{
+  public class RegisterViewModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "Go home Covid-19 you're not welcome here.")]
+    public string ConfirmPassword { get; set; }
+  }
+}
